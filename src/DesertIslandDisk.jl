c1c1bab3d43806dd09cb2d5a5e808753d6d26112
mahiki <mahiki@users.noreply.github.com>
2021-07-19 00:06:33 -0700

    minor order change of include

--- src/DesertIslandDisk.jl
+++ src/DesertIslandDisk.jl
@@ -3,15 +3,15 @@ module DesertIslandDisk
 
 export  @tostring
 export  @remindme
-export  stop_script_exection_and_return_to_repl_or_shell
 export  printschema
+export  stop_script_exection_and_return_to_repl_or_shell
 
 greet() = print("Hello World!")
 
 include("tostring.jl")
-include("repl_script_end_gracefully.jl")
 include("remindme.jl")
 include("printschema.jl")
+include("repl_script_end_gracefully.jl")
 
 
 end # module
