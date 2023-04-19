9462e91c0343c6a440e9a921d41e3d724d2c2ad1
mahiki <mahiki@users.noreply.github.com>
2023-04-19 14:54:13 -0700

    add addcb function

---
+++ src/clicalcs.jl
@@ -0,0 +1,28 @@
+#=  ================================================================================================
+    CLI Calculation Functions
+    little things to make testing/debugging data pipelines easier
+
+    author:     mahiki@users.noreply.github.com
+=   ===============================================================================================#
+
+
+"""
+    addcb(x::AbstractString)
+
+Add Clipboard. Parse the clipboard contents as a series of numbers, add them and return the sum.
+Example usage is copying a column of numbers from terminal and adding them.
+
+# Examples
+```jldoctest
+julia> clipboard("55 76 \n13, 22")
+julia> addcp()
+166
+```
+"""
+function addcb()
+    dlmregex = r"[\s,;|]"
+    x = clipboard()
+    m = split(x, dlmregex, keepempty = false)
+    total = sum(parse.(Float64, m))
+    return total
+end
